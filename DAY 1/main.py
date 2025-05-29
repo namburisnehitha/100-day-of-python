64e8878f4e332ff63acf230f64ccdea8265d52e8
namburisnehitha <namburisnehitha@gmail.com>
2025-05-29 17:02:13 +0530

    Create main.py

---
+++ DAY 1/main.py
@@ -0,0 +1,4 @@
+print("wellcome to band name genarator")
+city = input("whats the name of your city?\n")
+pet = input("whats the name of your pet\n")
+print("your band name could be: " + city + " "+ pet)
